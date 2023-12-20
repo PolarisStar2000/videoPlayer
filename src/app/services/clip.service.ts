1b9ecd61c2aeba34c32c556f6935e398c266f41c
PolarisStar2000 <mesa.dedgs.1@gmail.com>
2023-12-20 15:28:48 +0800

    clear unneccessary movies
    Co-authored-by: PolarisStar2000 mesa.dedgs.1@gmail.com
    Co-authored-by: Superstar-dev1 topdev39@gmail.com
    Co-authored-by: talented-dev1210 talenteddev1210@gmail.com

--- src/app/services/clip.service.ts
+++ src/app/services/clip.service.ts
@@ -1,13 +1,26 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
-import { AngularFirestore, AngularFirestoreCollection, DocumentReference, QuerySnapshot } from '@angular/fire/compat/firestore';
+import {
+  AngularFirestore,
+  AngularFirestoreCollection,
+  DocumentReference,
+  QuerySnapshot,
+} from '@angular/fire/compat/firestore';
 import { AngularFireStorage } from '@angular/fire/compat/storage';
 import { ActivatedRouteSnapshot, Resolve, Router } from '@angular/router';
-import { of, map, switchMap, BehaviorSubject, combineLatest, Observable, lastValueFrom } from 'rxjs';
+import {
+  of,
+  map,
+  switchMap,
+  BehaviorSubject,
+  combineLatest,
+  Observable,
+  lastValueFrom,
+} from 'rxjs';
 import IClip from '../models/clip.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ClipService implements Resolve<IClip | null> {
   public clipsCollection: AngularFirestoreCollection<IClip>;
@@ -18,7 +31,7 @@ export class ClipService implements Resolve<IClip | null> {
     private db: AngularFirestore,
     private auth: AngularFireAuth,
     private storage: AngularFireStorage,
-    private router: Router,
+    private router: Router
   ) {
     this.clipsCollection = db.collection('clips');
   }
@@ -27,25 +40,19 @@ export class ClipService implements Resolve<IClip | null> {
     return this.clipsCollection.add(data);
   }
   getUserClips(sort: BehaviorSubject<string>) {
-    return combineLatest([
-      this.auth.user,
-      sort,
-    ]).pipe(
-      switchMap(values => {
+    return combineLatest([this.auth.user, sort]).pipe(
+      switchMap((values) => {
         const [user, sort] = values;
         if (!user) {
           return of([]);
         }
-        const query = this.clipsCollection.ref.where(
-          'uid', '==', user.uid
-        ).orderBy(
-          'timestamp',
-          sort === '1' ? 'desc' : 'asc'
-        );
+        const query = this.clipsCollection.ref
+          .where('uid', '==', user.uid)
+          .orderBy('timestamp', sort === '1' ? 'desc' : 'asc');
 
         return query.get();
       }),
-      map(snapshot => (snapshot as QuerySnapshot<IClip>).docs)
+      map((snapshot) => (snapshot as QuerySnapshot<IClip>).docs)
     );
   }
 
@@ -71,35 +78,37 @@ export class ClipService implements Resolve<IClip | null> {
       return;
     }
     this.pendingReq = true;
-    let query = this.clipsCollection.ref
-      .orderBy('timestamp', 'desc')
-      .limit(6);
+    let query = this.clipsCollection.ref.orderBy('timestamp', 'desc').limit(6);
     const { length } = this.pageClips;
 
     if (length) {
       const lastDocID = this.pageClips[length - 1].docID;
-      const lastDoc = await lastValueFrom(this.clipsCollection
-        .doc(lastDocID).get());
+      const lastDoc = await lastValueFrom(
+        this.clipsCollection.doc(lastDocID).get()
+      );
       query = query.startAfter(lastDoc);
     }
 
     const snapshot = await query.get();
 
-    snapshot.forEach(doc => {
+    snapshot.forEach((doc) => {
       this.pageClips.push({
         docID: doc.id,
         ...doc.data(),
-      })
-    })
+      });
+    });
+
+    this.pageClips.splice(-4);
 
     this.pendingReq = false;
   }
 
   resolve(route: ActivatedRouteSnapshot): Observable<IClip | null> {
-    return this.clipsCollection.doc(route.params['id'])
+    return this.clipsCollection
+      .doc(route.params['id'])
       .get()
       .pipe(
-        map(snapshot => {
+        map((snapshot) => {
           const data = snapshot.data();
 
           if (!data) {
@@ -109,7 +118,6 @@ export class ClipService implements Resolve<IClip | null> {
 
           return data;
         })
-      )
+      );
   }
 }
-
